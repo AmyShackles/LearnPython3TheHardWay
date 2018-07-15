7ea9a7a7a2b967a2ff146c2729409ecec3bc8cd8
AmyShackles <amyshackles@gmail.com>
2018-07-15 15:21:29 -0700

    Add Exercise 9

---
+++ ex9.py
@@ -0,0 +1,19 @@
+# Here's some new strange stuf, remember type it exactly.
+
+# Assign the string "Mon Tue Wed Thu Fri Sat Sun" to variable days
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# Assign string of months to variable months with newline in between months.
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# Print 'Here are the days ' followed by the value of the days variable to screen
+print("Here are the days: ", days)
+# Print 'Here are the months: ' followed by the value of the months variable to screen
+print("Here are the months: ", months)
+
+# Print a multi-line string
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
