d3a9473dea21d17d165dfaab00d6939816f17d4c
AmyShackles <amyshackles@gmail.com>
2018-07-15 16:33:56 -0700

    Added Exercise 11

---
+++ ex11.py
@@ -0,0 +1,9 @@
+#Adding the end=' ' ensures that it doesn't print the response on a new line
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
