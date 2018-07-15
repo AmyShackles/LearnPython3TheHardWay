582158cf87e915c46d38fcbe107e01275889993f
AmyShackles <amyshackles@gmail.com>
2018-07-15 16:40:06 -0700

    Added Exercise 12

---
+++ ex12.py
@@ -0,0 +1,6 @@
+# Different way of writing code from ex11
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"So you're {age} old, {height} tall and {weight} heavy.")
