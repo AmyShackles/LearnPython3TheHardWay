650640ba69c0187078366c1de26d82e4fcec5a72
AmyShackles <amyshackles@gmail.com>
2018-07-15 16:28:56 -0700

    Added Exercise 10

---
+++ ex10.py
@@ -0,0 +1,22 @@
+#assigns the value of a tab followed by "I'm tabbed in" to variable tabby_cat
+tabby_cat = "\tI'm tabbed in."
+# assigns the value of "I'm split" followed by a new line followed by "on a line" to variable persian_cat
+persian_cat = "I'm split\non a line."
+#assigns the value of "I'm \ a \ cat" to variable backslash_cat
+backslash_cat = "I'm \\ a \\ cat."
+
+#assigns the value of a multi-line formatted string to variable fat_cat
+fat_cat = f"I'll do a list: \
+\n\t* Cat food \
+\n\t* Fishies \
+\n\t* Catnip\n\t* Grass \
+"
+
+# prints the value of variable tabby_cat to screen
+print(tabby_cat)
+# prints the value of variable persian_cat to screen
+print(persian_cat)
+# prints the value of variable backslash_cat to the screen
+print(backslash_cat)
+#prints the value of variable fat_cat to the screen
+print(fat_cat)
