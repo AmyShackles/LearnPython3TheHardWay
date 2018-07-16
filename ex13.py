e50f0bca0ce31f6ba876e5718ef420832ace0f04
AmyShackles <amyshackles@gmail.com>
2018-07-15 17:00:11 -0700

    Added Exercise 13

---
+++ ex13.py
@@ -0,0 +1,10 @@
+from sys import argv
+# read the WYSS section for how to run this
+# this takes the arguments in argv and assigns them to variables in order
+print(type(argv))
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
