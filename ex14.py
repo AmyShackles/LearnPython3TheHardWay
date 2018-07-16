15570b0ccc89d181f0c3bdffff09651b26b96329
AmyShackles <amyshackles@gmail.com>
2018-07-15 17:59:10 -0700

    Added Exercise 14

---
+++ ex14.py
@@ -0,0 +1,21 @@
+from sys import argv
+
+script, user_name, nickname = argv
+prompt = '=^_^=****> '
+
+print(f"Hey {user_name}, can I call you {nickname}?  I'm the {script} script.")
+print("I'd like to ask you a few questions.")
+print(f"Do you like me {user_name}?")
+likes = input(prompt)
+
+print(f"Where do you live {user_name}?")
+lives = input(prompt)
+
+print("What kind of computer do you have?")
+computer = input(prompt)
+
+print(f"""
+Alright, so you said {likes} about liking me.
+You live in {lives}.  Not sure where that is.
+And you have a {computer} computer.  Nice.
+""")
