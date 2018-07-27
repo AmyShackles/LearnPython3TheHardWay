4e175d58dfe8c7295ebfbee3947e944b35e52f8c
AmyShackles <amyshackles@gmail.com>
2018-07-27 14:14:30 -0700

    Added Exercises 32-37

---
+++ ex34.py
@@ -0,0 +1,19 @@
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# 1. The animal at 1 is 'python3.6' and is the second animal.
+# 2. The third animal is 'peacock' and is at index 2.
+# 3. The first animal is 'bear' and is at index 0.
+# 4. The animal at 3 is 'kangaroo' and is the fourth animal.
+# 5. The fifth animal is 'whale' and is at index 4.
+# 6. The animal at 2 is 'peacock' and is the third animal.
+# 7. The sixth animal is 'platypus' and is at index 5.
+# 8. The animal at 4 is 'whale' and is the fifth animal.
+
+print("The animal at 1 is ", animals[1], "and is the second animal")
+print("The third animal is ", animals[2], "and is at index 2")
+print("The first animal is ", animals[0], "and is at index 0.")
+print("The animal at 3 is ", animals[3], "and is the fourth animal.")
+print("The fifth animal is ", animals[4], " and is at index 4.")
+print("The animal at 2 is ", animals[2], "and is the third animal.")
+print("The sixth animal is ", animals[5], "and is at index 5.")
+print("The animal at 4 is ", animals[4], "and is the fith animal.")
